135019bda9e1cffbf22ee37a10b70b9d189eddbc
Jonas Wielicki <j.wielicki@sotecware.net>
2017-06-07 12:47:56 +0200

    Add plugin to show time of execution of a line

---
+++ plugins/execdate/execdate.plugin.zsh
@@ -0,0 +1,8 @@
+function _reset-prompt-and-accept-line {
+  old_prompt="$PROMPT"
+  PROMPT="[%D{%H:%M:%S}] ${PROMPT}"
+  zle reset-prompt
+  PROMPT="$old_prompt"
+  zle .accept-line     # Note the . meaning the built-in accept-line.
+}
+zle -N accept-line _reset-prompt-and-accept-line
