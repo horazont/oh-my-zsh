9d33663e8dba9557a89b978d25d7de83660543d6
Jonas Wielicki <j.wielicki@sotecware.net>
2017-07-06 10:24:20 +0200

    Change execdate colour

--- plugins/execdate/execdate.plugin.zsh
+++ plugins/execdate/execdate.plugin.zsh
@@ -1,6 +1,6 @@
 function _reset-prompt-and-accept-line {
   old_prompt="$PROMPT"
-  PROMPT="[%D{%H:%M:%S}] ${PROMPT}"
+  PROMPT="%{$fg_bold[black]%}[%D{%H:%M:%S}] ${PROMPT}"
   zle reset-prompt
   PROMPT="$old_prompt"
   zle .accept-line     # Note the . meaning the built-in accept-line.
